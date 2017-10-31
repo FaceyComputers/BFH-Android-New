0d70ce7fe70f51b18f2a1b6fe6328bb8d1bd42f2
Joshua Moore <joshuamoore235@gmail.com>
2017-10-31 13:41:08 -0600

    Fixed linked images

--- BevFacey/app/src/main/java/com/jmoore/bevfacey/ArticleHolder.java
+++ BevFacey/app/src/main/java/com/jmoore/bevfacey/ArticleHolder.java
@@ -1,16 +1,16 @@
 package com.jmoore.bevfacey;
 
 import android.app.Activity;
-import android.graphics.Color;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import com.squareup.picasso.Picasso;
 
 import java.util.regex.Pattern;
 
@@ -18,8 +18,6 @@ class ArticleHolder extends RecyclerView.ViewHolder {
     private final Activity context;
     private boolean loaded = false;
     private final LinearLayout layout;
-    private final String[] imageUrlPattern = {"<img alt=\"\" src=\"", "\">"}; //Pattern for finding image URLs
-    private boolean imageLoaded = false;
 
     ArticleHolder(Activity context, View itemView) {
         super(itemView);
@@ -27,53 +25,33 @@ class ArticleHolder extends RecyclerView.ViewHolder {
         layout = itemView.findViewById(R.id.articleLayout);
     }
 
-    private void updateImage() {
-
-    }
-
     void bindArticle(String content) {
         if(!loaded) {
-            ImageView imageView = new ImageView(context);
-            imageView.setLayoutParams(new LinearLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            imageView.setPadding(0, 0, 0, 0);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            imageView.setAdjustViewBounds(true);
-            imageView.setBackgroundColor(Color.WHITE);
+            WebView webView = new WebView(context);
+            Display display = context.getWindowManager().getDefaultDisplay();
+            float width = display.getWidth();
+            float scale = webView.getScale();
+            int size = (int)(width / scale);
 
-            if(content.contains("<img alt")) {
-                String imgId = MainActivity.getFromPatternStatic(imageUrlPattern, content);
-                if(!imgId.contains("bevfacey.ca")) {
-                    imgId = "https://bevfacey.ca" + imgId;
+            webView.getSettings().setLoadsImagesAutomatically(true);
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+            webView.setVerticalScrollBarEnabled(false);
+            webView.setHorizontalScrollBarEnabled(false);
+            webView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    return (event.getAction() == MotionEvent.ACTION_MOVE);
                 }
-                Log.e("IMGID",imgId);
-                try {
-                    Picasso.with(context).load(imgId).into(imageView, new com.squareup.picasso.Callback() {
-                        @Override
-                        public void onSuccess() {
-                            //layout.addView(imageView);
-                            Log.i("ImageLoaded", "Image loaded successfully");
-                        }
+            });
 
-                        @Override
-                        public void onError() {
-                            Log.e("ImageFailedToLoad", "An image failed to load properly");
-                        }
-                    });
-                } catch (Exception ignored) {
-                    Log.e("ImageFailedToLoad", "An image failed to load properly: " + ignored.getMessage());
-                }
-            }
+            content = content.replaceAll(Pattern.quote("img alt=\"") + "(.*?)" +
+                    Pattern.quote("\" src=\"/"), "img alt=\"\" style=\"margin: 0; " +
+                    "padding: 0\" width=" + size + "px src=\"https://bevfacey.ca/");
 
-            layout.addView(imageView);
+            webView.loadData(content, "text/html; charset=utf-8", "UTF-8");
 
-            content = content.replaceAll(Pattern.quote("img alt=\"\""), "img");
-            System.out.println(content);
-            WebView textView = new WebView(context);
-            textView.getSettings().setLoadsImagesAutomatically(true);
-            textView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-            textView.loadData(content, "text/html; charset=utf-8", "UTF-8");
-            layout.addView(textView);
+            layout.addView(webView);
             loaded = true;
         } else {
             Log.i("Loaded", "View already loaded");
